59e951d75469d796133e971aac82dd175a300f2c
ArpanaMishra94 <arpanamishra394@gmail.com>
2024-06-10 00:13:33 +0530

    memoize update

--- memoization.js
+++ memoization.js
@@ -1,3 +1,26 @@
+function memoizedAddTo256(){
+    var cache = {};
+  
+    return function(num){
+      if(num in cache){
+        console.log("cached value");
+        return cache[num]
+      }
+      else{
+        cache[num] = num + 256;
+        return cache[num];
+      }
+    }
+  }
+  var memoizedFunc = memoizedAddTo256();
+  
+  memoizedFunc(20); // Normal return
+  memoizedFunc(20); // Cached return
+
+
+// or
+
+
 let sum = 0;
 const calc = (n) => {
     for(let i = 0; i <= n; i++) {
